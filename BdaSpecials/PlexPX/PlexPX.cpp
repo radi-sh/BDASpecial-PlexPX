826417e8feb04df0f7af8d5dcc69448c457764e5
radi_sh <masamory7@gmail.com>
2019-02-16 11:00:47 +0900

    [PlexPX] Debug ビルド時にメモリリーク検出を行うようにした

--- BdaSpecials/PlexPX/PlexPX.cpp
+++ BdaSpecials/PlexPX/PlexPX.cpp
@@ -1,3 +1,9 @@
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #include "common.h"
 
 #include "PlexPX.h"
@@ -127,6 +133,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
+#ifdef _DEBUG
+		::_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 		// モジュールハンドル保存
 		hMySelf = hModule;
 		break;
